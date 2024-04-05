016e22a1e92d2c0094ada69d789137bb6b2e363f
Brandon <bran2miz@gmail.com>
2024-04-05 12:56:58 -0700

    lab28 almost finished video, working on tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 import './App.scss';
@@ -18,38 +18,93 @@ const App = () => {
     data: null,
     requestParams: {}
   });
-// useState is a special function that can accept an argument
-// the argument represents the starting value of the state
-// returns an array of a getter and setter
+  // useState is a special function that can accept an argument
+  // the argument represents the starting value of the state
+  // returns an array of a getter and setter
+
+
+  // listen for the state (newState.requestParams) when it changes (url and method, maybe body)
+  // when they change, make a http request 
+  // update the DATA (appState.data) to the new values. 
+
+
+  // use a useEffect to do this
+  //OPTION 1 (option 2 in README.md)
+  useEffect(() => {
+    // console.log('this is a callback function');
+    // can do anything
+    // use wrap the async function in parenthesis so that it turns it into an anonymous function
+
+    // if there isn't a url, don't run this and return to the callback.
+    if (!newState.requestParams.url) return;
+
+    // if data return newState.
+    // early exit gate
+    if (newState.data && Object.keys(newState.data)) return;
+    // run this function only if I don't have data...:
+    (async () => {
+      // make the request
+      const url = newState.requestParams.url
+
+      const method = newState.requestParams.method
+
+      // make a dummy request
+      const request = {
+        count: 2,
+        results: [
+          { name: "fake thing 1", url: "http://fakethings.com/1" },
+          { name: "fake thing 2", url: "http://fakethings.com/2" },
+        ],
+      };
+      // make the request, get back data
+      // ...newState is a spread operator
+      // takes the object and spreads it apart
+      // {data, requestParams}
+      // {...newState, pizza: "yum"}
+      // newState is now {data, requestParams, pizza: "yum"}
+      // {data: {fjfjfje:}, requestParams: {url: "yaya", method: "GET"}, data {}}
+
+      // new value of data will be an empty object or the things back from the request you make
+
+      setState({ ...newState, data: request.data })
+
+      // be very careful that you don't create a circular dependency where the state of the the thing you are watching changes every time the function runs
+    })();
+    return () => {
+      console.log('component unmounts');
+    }
+  }, [newState])
 
   const callApi = (requestParams) => {
     // mock output
-    const data = {
-      count: 2,
-      results: [
-        {name: "fake thing 1", url: "http://fakethings.com/1"},
-        {name: "fake thing 2", url: "http://fakethings.com/2"},
-      ],
-    };
+    // const data = {
+    //   count: 2,
+    //   results: [
+    //     {name: "fake thing 1", url: "http://fakethings.com/1"},
+    //     {name: "fake thing 2", url: "http://fakethings.com/2"},
+    //   ],
+    // };
     // setting form data as requestParams
     //setState of requestParams (an empty object) to formData which is object with keys of method, url, and reqBody. 
-    setState({data, requestParams})
+    setState({ data: {}, requestParams })
   }
 
 
-    return (
-     <>
-        <Header />
-        <div>Request Method: {newState.requestParams.method}</div>
-{/* use the newState which is the state object, the requestParams which the empty object that now has the formData, and grab the url key to render the url value from the form */}
-        <div>URL: {newState.requestParams.url}</div>
-        {newState.requestParams.body && <div>Body: {newState.requestParams.body}</div>}
-        <Form handleApiCall={callApi} />
-        <Results data={newState.data} />
-        <Footer />
+
+
+  return (
+    <>
+      <Header />
+      <div>Request Method: {newState.requestParams.method}</div>
+      {/* use the newState which is the state object, the requestParams which the empty object that now has the formData, and grab the url key to render the url value from the form */}
+      <div>URL: {newState.requestParams.url}</div>
+      {newState.requestParams.body && <div>Body: {newState.requestParams.body}</div>}
+      <Form handleApiCall={callApi} />
+      <Results data={newState.data} />
+      <Footer />
     </>
-    );
-  
+  );
+
 }
 
 export default App;
